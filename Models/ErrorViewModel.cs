4f318b6217729cf52f47db2d623ed26f3e95587a
Firoz Ebna Jobaier <f.e_jobaier@hotmail.com>
2022-04-22 17:02:56 +0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace smsApps.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
